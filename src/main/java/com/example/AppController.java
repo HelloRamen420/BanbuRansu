b23ad2f4f490d33a864bef4824bdf205446ebe08
helloramen <onipanman888@gmail.com>
2024-12-09 20:22:01 +0900

    Create BanbuRansu

---
+++ src/main/java/com/example/AppController.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import javafx.fxml.FXML;
+
+public class AppController {
+    @FXML
+    private void explanation() throws Exception {
+        System.out.println("そうだね");
+    }
+}
---
+++ src/main/java/com/example/make/MakeControl.java
@@ -0,0 +1,5 @@
+package com.example.make;
+
+public class MakeControl {
+
+}
