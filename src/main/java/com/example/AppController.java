1c7b69650715dc0cb5791cd33fce3be2785dcd8e
helloramen <onipanman888@gmail.com>
2024-12-11 15:36:03 +0900

    実際何も進んでないのに階層構造を間違えて半日消費したコミット

--- src/main/java/com/example/AppController.java
+++ src/main/java/com/example/AppController.java
@@ -1,10 +1,20 @@
 package com.example;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class AppController {
+
+    /*
+     * ハイパーリンクと対応してます。
+     * 「万物乱数ちゃん」とは何か、とかが書いてあります。
+     */
     @FXML
     private void explanation() throws Exception {
-        System.out.println("そうだね");
+        Stage explanStage = new Stage();
+        Scene explanScene = new Scene(App.loadFXML("explanation"), 600, 400);
+        explanStage.setScene(explanScene);
+        explanStage.showAndWait();
     }
 }
