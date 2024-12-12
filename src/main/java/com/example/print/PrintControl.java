1d7b91ee0902f426be0a3e53d4326c792f818f37
helloramen <onipanman888@gmail.com>
2024-12-12 19:34:40 +0900

    JavaFXでライブラリを使うときはモジュールにぶち込んで使いましょうね、ま知らんけどのコミット

---
+++ src/main/java/com/example/print/PrintControl.java
@@ -0,0 +1,8 @@
+package com.example.print;
+
+import com.fasterxml.jackson.core.JsonNode;
+import com.fasterxml.jackson.core.ObjectMapper;
+
+public class PrintControl {
+    public static ArrayList jsonJavaWalcome()
+}
